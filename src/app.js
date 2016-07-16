6b474457db5b504af153ef41075344d8b66ee629
Cindy Salinas <cindysalinas15@gmail.com>
2016-07-15 21:54:15 -0400

    added root component

---
+++ src/app.js
@@ -0,0 +1,36 @@
+import 'zone.js/dist/zone';
+import 'reflect-metadata';
+import 'babel-core';
+
+import { Component, View, provide, enableProdMode } from '@angular/core';
+import { bootstrap } from '@angular/platform-browser-dynamic';
+import { provideRouter, RouterConfig, ROUTER_DIRECTIVES } from '@angular/router';
+import {APP_BASE_HREF} from '@angular/common';
+
+import { HelloComponent } from './hello.component';
+import { AboutComponent } from './about.component';
+
+const routes: RouterConfig = [
+	{ path: '', component: HelloComponent },
+	{ path: 'about', component: AboutComponent }
+];
+@Component({
+	selector: 'hello-app',
+	directives: [ROUTER_DIRECTIVES],
+	template: `
+		<ul>
+			<li><a [routerLink]="['/']">Home</a></li>
+			<li><a [routerLink]="['about']">About</a></li>
+		</ul>
+		<router-outlet></router-outlet>
+	`
+})
+
+class HelloApp { }
+//production mode
+enableProdMode();
+//start app
+bootstrap(HelloApp, [
+	provideRouter(routes)
+])
+.catch(err => console.error(err));
